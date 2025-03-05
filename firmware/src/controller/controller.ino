4e8a3cd142e7d9a23ee0f85a0ff689256668be62
Kyle James <94993984+kj-49@users.noreply.github.com>
2025-03-05 09:46:58 -0700

    Add comments to main loop

--- firmware/src/controller/controller.ino
+++ firmware/src/controller/controller.ino
@@ -23,14 +23,14 @@ void setup() {
 }
 
 void loop() {
-
+    // Update the application state (take ADC readings etc)
     state_manager_update_sensors();
-    
+    // Determine the next FSM state based on the new updated state
     state_manager_update_mode();
-
+    // Make hardware updates
     state_manager_apply_hardware_updates();
-    
+    // Update display
     display_update();
-
+    
     delay(LOOP_DELAY_MS);
 }
