ac7d5f81359dc82a45a8eeae358d77ce2dd8b67d
Kyle James <ksjames49@gmail.com>
2025-03-07 08:13:36 -0700

    Modify charge voltage pin due to hardware damages

--- firmware/src/controller/gpio.h
+++ firmware/src/controller/gpio.h
@@ -10,7 +10,9 @@ const int TOTAL_CELL_ADC_PIN = A0;
 const int LOWER_CELL_ADC_PIN = A1;
 const int CELL1_TEMP_PIN = A2;     
 const int CELL2_TEMP_PIN = A3;
-const int CHARGE_VOLTAGE_PIN = A6;
+
+/* A6 not functional on current board */
+const int CHARGE_VOLTAGE_PIN = A3;
 
 const int LOWER_CELL_DISCHARGE_PIN = PD2;
 const int UPPER_CELL_DISCHARGE_PIN = PD4;
