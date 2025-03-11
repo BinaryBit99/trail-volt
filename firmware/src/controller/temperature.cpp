4960755589acfbe2894ffdab91005fc52d2d125c
Evan Barker <111829951+BinaryBit99@users.noreply.github.com>
2025-03-10 22:10:52 -0600

    Update temperature.cpp
    added in function; needs to be looked over

--- firmware/src/controller/temperature.cpp
+++ firmware/src/controller/temperature.cpp
@@ -1,6 +1,12 @@
 #include "temperature.h"
 #include "adc.h"
 
+
+#define ADC_MAX   1023.0f   // Arduino Nano has a 10-bit ADC (0-1023)
+
+typedef uint16_t u16; // Use unsigned 16 bit readings for NANO
+
+
 float get_resistance(int pin) {
     float voltage = read_from_adc(pin, THERMISTOR_ADC_DIVISION);
 
@@ -33,3 +39,25 @@ float temperature_get_lower_cell() {
      
     return 0;
 }
+
+// The below function returns the temp. of thermistor based on the varying resistance.
+
+float getTempfromThermistor(int pin) {
+    const float R0 = 10000.0f;
+    const float R_fixed = 40000.0f;
+    const float Beta = 3380.0f;            // These were found in datasheet.
+    const float T0 = 298.15f;
+    
+    float r_ntc = get_resistance(pin);        // This seems more abstract; less clutter for code at the expense of less modularity.
+    
+    if (adc_value <= 0 || adc_value >= ADC_MAX) return -999.0f; // Use this right away to maintain 'safe' bounds - can adjust if needed. I set ADC_MAX to 5.
+
+    // Use a Steinhart equation for conversions.
+    float steinhart = (1.0f / T0) + (1.0f / BETA) * log(r_ntc / R0);
+    float t_kelvin = 1.0f / steinhart;
+    float t_celsius = t_kelvin - 273.15f;
+    
+    return t_celsius;
+
+}
+
